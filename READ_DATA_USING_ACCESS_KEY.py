6df318f30105fb936eeadf57c1182c21c7c4f2ef
rms181800@gmail.com <rms181800@gmail.com>
2026-05-11 16:04:58 +0000

    mODIFIED NOTEBOOK

--- READ_DATA_USING_ACCESS_KEY.py
+++ READ_DATA_USING_ACCESS_KEY.py
@@ -1,3 +1,18 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC # THIS NOTEBOOK IS USED TO READ DATA FROM ADLS USING ACCESS KEY
+
+# COMMAND ----------
+
+lst = [1,2,3,4,5]
+even = []
+
+for i in lst:
+    if i%2 == 0:
+        even.append(i)
+print(even)
+
+# COMMAND ----------
+
+SEELCT * FROM CUSTOMER DF
+WHERE CITY = "ABCD"
